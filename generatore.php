7a9b3899b1a52e4cf72757a7b31cc0a23b2af0e4
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-06-12 21:49:38 +0200

    M2

--- generatore.php
+++ generatore.php
@@ -7,23 +7,28 @@
 </head>
 <body>
 <?php
-       
-        
         if (isset($_GET['length']) && is_numeric($_GET['length'])) {
             //RECUPERO LA LUNGHEZZA DELLA PSSWRD
             $length = intval($_GET['length']);
             
-            // CREO LA STRINGA
-            $password = '';
-            for ($i = 0; $i < $length; $i++) {
-                $password .= 'X';
-            }
-            
-            //STAMPO PASSWRD
-            echo "<p>La tua password è: $password</p>";
-        } else {
-            echo "<p>Parametro 'length' non valido o mancante. Si prega di specificare una lunghezza valida.</p>";
-        }
+             //STRINGA DEI CARATTERI CONSENTITI
+             $caratteri = 'abcdefghijklmnopqrstuvwxyz0123456789!?-+£$%=';
+             $lunghezza = strlen($caratteri); //LUNGHEZZA DELLA STRINGA
+             
+             // GENERO PASSWORD CASUALE
+             $password = '';
+             for ($i = 0; $i < $length; $i++) {
+                 //AD OGNI ITERAZIONE PRENDE UN INDICE CASUALE
+                 $indiceCasuale = rand(0, $lunghezza - 1);
+                 //AGGIUNGE IL CARATTERE ALLA PASSWORD
+                 $password .= $caratteri[$indiceCasuale];
+             }
+             
+             // STAMPO IN PAGINA
+             echo "<p>La tua password è: $password</p>";
+         } else {
+             echo "<p>Parametro 'length' non valido o mancante. Si prega di specificare una lunghezza valida.</p>";
+         }
     ?>
     <a href="input.php">Torna indietro</a>
 </body>
